d1c773b4cce9d22b842fbb7d95d6f9b04e263daa
wjj329 <wjj2187@naver.com>
2024-02-22 19:55:47 +0900

    fix. 슈팅 간격 코드 추가

--- Assets/1. Scripts/PlayerShooting.cs
+++ Assets/1. Scripts/PlayerShooting.cs
@@ -7,11 +7,15 @@ public class PlayerShooting : MonoBehaviour
     public GameObject bulletPref; // 총알 프리팹 변수
     public GameObject ShootEffectPref; // 슈팅 효과 프리팹 변수
 
+    public float shootingInterval = 0.2f; // 총알 발사 간격
+    private float lastShootTime;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false; 
-        Cursor.lockState = CursorLockMode.Confined; 
+        Cursor.lockState = CursorLockMode.Confined;
+        lastShootTime = -shootingInterval; 
     }
 
     // Update is called once per frame
@@ -19,21 +23,30 @@ public class PlayerShooting : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            Ray ray = Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f));
-            RaycastHit hit;
-
-            Physics.Raycast(ray, out hit);
-
-            if (Physics.Raycast(ray, out hit))
+            if (Input.GetMouseButton(0) && Time.time - lastShootTime > shootingInterval)
             {
-                GameObject shootEffect = Instantiate(ShootEffectPref, hit.point + hit.normal * 0.01f, Quaternion.LookRotation(hit.normal));
-                shootEffect.transform.SetParent(hit.transform);
+                Shoot(); // 총을 발사하는 함수 호출
+                lastShootTime = Time.time; // 마지막 총 발사 시간 갱신
             }
+        }
+    }
 
-            if (hit.transform.tag == "Enemy")
-            {
-                hit.transform.SendMessage("Damaged", 10); 
-            }
+    void Shoot()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f));
+        RaycastHit hit;
+
+        Physics.Raycast(ray, out hit);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject shootEffect = Instantiate(ShootEffectPref, hit.point + hit.normal * 0.01f, Quaternion.LookRotation(hit.normal));
+            shootEffect.transform.SetParent(hit.transform);
+        }
+
+        if (hit.transform.tag == "Enemy")
+        {
+            hit.transform.SendMessage("Damaged", 10);
         }
     }
 }
